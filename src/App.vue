fcd3362edca5bf798d3042e3f15c1e659cb58fbe
Matthias Hryniszak <padcom@gmail.com>
2022-11-09 18:20:46 +0100

    Added example for testing performance

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div>
     <fieldset>
       <p>Center: {{ center }}, Zoom: {{ zoom }}</p>
-      <button @click="createMarkers" :disabled="markers.length > 0">Create markers</button>
-      <button @click="removeMarkers" :disabled="markers.length === 0">Delete markers</button>
-      <button @click="test" :disabled="markers.length > 0">Stress-test marker generation</button>
     </fieldset>
     <div ref="map" class="map"></div>
   </div>
@@ -15,9 +12,12 @@ import { defineComponent } from 'vue'
 import { EventEmitter } from 'events'
 import L, { LatLng } from 'leaflet'
 import data from './random-coordinates.json'
-import voivodeships from './wojewodztwa-min.geo.json'
 import counties from './powiaty-min.geo.json'
 import { GeoJsonObject } from 'geojson'
+import { v4 as uuid } from 'uuid'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet.markercluster'
 
 type Nullable<T> = T | null
 
@@ -38,57 +38,10 @@ const MB_URL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_t
 const ICON_ON = L.icon({ iconUrl: '/default-marker-highlighted.png' })
 const ICON_OFF = L.icon({ iconUrl: '/default-marker.png' })
 
-class MyMarker extends EventEmitter {
-  public readonly data: L.Marker
-
-  constructor(
-    public readonly map: L.Map,
-    public readonly id: string,
-    coords: LatLng,
-  ) {
-    super()
-    this.data = L
-      .marker(coords)
-      .setIcon(ICON_OFF)
-      .on('click', e => {
-        this.click()
-      })
-      .on('mouseover', e => {
-        this.highlight()
-      })
-      .on('mouseout', e => {
-        this.unhighlight()
-      })
-      .addTo(this.map)
-  }
-
-  remove() {
-    this.removeAllListeners()
-    this.data.clearAllEventListeners
-    this.data.remove()
-  }
-
-  click() {
-    this.emit('click', this)
-    console.log('marker clicked', this)
-  }
-
-  highlight() {
-    this.data.setIcon(ICON_ON)
-    console.log('marker hovered', this)
-  }
-
-  unhighlight() {
-    this.data.setIcon(ICON_OFF)
-    console.log('marker left', this)
-  }
-}
-
 export default defineComponent({
   data() {
     return {
       map: null as Nullable<L.Map>,
-      markers: [] as MyMarker[],
       center: HOME,
       zoom: MAX_ZOOM
     }
@@ -105,28 +58,48 @@ export default defineComponent({
       zoomOffset: -1,
       attribution: MB_ATTRIB
     })
-    // @ts-ignore
-    const voivodeship = L.geoJSON(voivodeships as GeoJsonObject, { interactive: true })
-      .on('mouseover', (e) => {
-        voivodeship.setStyle({ opacity: 0.5 })
+
+    const NUMBER_OF_MARKERS = 50000
+
+    console.time('Creating ' + NUMBER_OF_MARKERS + ' random marker points')
+    const markers = new Array(NUMBER_OF_MARKERS).fill({ lat: 0, lon: 0, id: 0 }).map(marker => {
+      const r = 1 * Math.sqrt(Math.random())
+      const theta = Math.random() * 2 * Math.PI
+
+      return {
+        id: uuid(),
+        lat: HOME.lat + r * Math.cos(theta),
+        lon: HOME.lng + r * Math.sin(theta) * 2,
+      }
+    })
+    console.timeEnd('Creating ' + NUMBER_OF_MARKERS + ' random marker points')
+
+    console.time('Creating cluster')
+    const clusters = L.markerClusterGroup()
+    const toMarker = (marker: any) => L
+      .marker([ marker.lat, marker.lon ], { icon: ICON_OFF })
+      .on('mouseover', e => { e.target.setIcon(ICON_ON) })
+      .on('mouseout', e => { e.target.setIcon(ICON_OFF) })
+
+    markers
+      .map(toMarker)
+      .forEach(marker => {
+        clusters.addLayer(marker)
       })
-      .on('mouseout', (e) => {
-        voivodeship.setStyle({ opacity: 0.8 })
-      })
-      .on('add', e => console.log('add', e.target))
-    // voivodeship.
-    // const county = L.geoJSON(counties)
+    console.timeEnd('Creating cluster')
 
-
-
+    console.time('Creating map')
     map
-      // .addLayer(osm)
-      .addLayer(satellite)
-      .addLayer(voivodeship)
+      .addLayer(osm)
+      // .addLayer(satellite)
+      .addLayer(clusters)
       // .addLayer(L.marker(this.center).on('click', () => { map.setView(HOME, MAX_ZOOM) }))
       .on('zoom', () => this.zoom = map.getZoom())
       .on('move', () => this.center = map.getCenter())
 
+    console.timeEnd('Creating map')
+
+    // clusters.addTo(map)
 
     // @ts-ignore
     window.L = L
@@ -140,28 +113,9 @@ export default defineComponent({
     this.map = null
   },
   methods: {
-    async test() {
-      for (let i = 0; i < 10; i++) {
-        this.createMarkers()
-        await this.$nextTick()
-        this.removeMarkers()
-        await this.$nextTick()
-      }
-    },
     createMarkers() {
-      console.time('Adding markers')
-      data.forEach(point => {
-        const marker = new MyMarker(this.map as any, point.id, L.latLng({ lat: point.lat, lng: point.lon }))
-        marker.on('click', () => { console.log('marker clicked') })
-        this.markers.push(marker)
-      })
-      console.timeEnd('Adding markers')
     },
     removeMarkers() {
-      console.time('Removing markers')
-      this.markers.forEach(marker => { marker.remove() })
-      this.markers = []
-      console.timeEnd('Removing markers')
     }
   }
 })
